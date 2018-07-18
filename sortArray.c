965ae1a6c72ee217b4e042f4bdf9001107503ad7
JulianSeepersaud <julian89624@gmail.com>
2018-07-17 22:09:25 -0400

    Search {O(log(n))} + Sort O(n^2)

--- sortArray.c
+++ sortArray.c
@@ -2,6 +2,59 @@
 #include <stdlib.h>
 
 
+void printArray(int arraySize, int* theArray){
+				for (int i = 0; i < arraySize; ++i)
+			{
+				printf("%d, ", theArray[i]);
+			}
+}
+
+int binarySeach(int arraySize, int* theArray, int key){
+	if (arraySize == 0)
+	{
+		return -1;
+	}
+	if (arraySize == 1 && theArray[0]== key)
+	{
+		return 0; 
+	}
+	if (arraySize== 2)
+	{
+		if (theArray[0]== key)
+		{
+			return 0;
+		}
+		if (theArray[1] == key)
+		{
+			/* code */
+			return 1; 
+		}
+	}
+	else{
+
+		int lower = 0;
+		int max = arraySize-1;
+		int middle = arraySize/2;
+	
+		while(middle!= lower || middle!= max){
+		if (theArray[middle] == key)
+		{
+
+			return middle; // value found, so return middle index
+		}
+		else if (theArray[middle] > key){
+			max= middle-1;
+			middle = max/2;
+		}
+		else if(theArray[middle]  < key){
+			lower = middle+1;
+			middle = (max + lower ) /2 ; 
+		}
+	}
+}
+	return -1; // not found
+}
+
 int* sort(int arraySize, int* theArray){
 	if(arraySize == 0 || arraySize == 1){
 		return theArray;
@@ -10,33 +63,32 @@ int* sort(int arraySize, int* theArray){
 		for (int i = 0; i < arraySize; ++i)
 		{
 			
-			for (int j = 1; j < arraySize; ++j)
+			for (int j = i+1; j < arraySize; ++j)
 			{
 				if (theArray[j]<theArray[i])
 				{
-
+					
 					int swap = theArray[i];
 					theArray[i] = theArray[j];
 					theArray[j] = swap;
+					
 				}
 			}
 		}
-		for (int i = 0; i < 6; ++i)
-			{
-				printf("%d, ", theArray[i]);
-			}
-
+		
 		return theArray;
 	}
 }
 int main(int argc, char *argv[]){
 
-	int a[] = {5, 54, 2, 1, 555, 334};
-	int* ca = sort(6 , a);
-	for (int i = 0; i < 5; ++i)
-	{
-		// printf("%d", ca[i]);
-	}
+	int a[] = {5, 4};
+	int arraySIZE =2 ;
+	int* ca = sort(arraySIZE , a);
+	printArray(arraySIZE,ca);
+	
+printf("\n");
+	int x = binarySeach(arraySIZE , a , 4);
+	printf("%d\n", x);
 return 0;
 
 }
